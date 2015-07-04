878896c023053091ae89745a2ab9da967ab5225e
Rodolfo Acuna <racuna@growthaccelerationpartners.com>
2015-07-04 15:46:05 -0600

    Exercise 00

--- workshop/warmup/hello_world.py
+++ workshop/warmup/hello_world.py
@@ -22,6 +22,13 @@ while 1:
         elif event.type == pygame.KEYDOWN:
             pass
 
+
+    logorect = logorect.move(speed)
+    if logorect.left < 0 or logorect.right > width:
+        speed[0] = -speed[0]
+    if logorect.top < 0 or logorect.bottom > height:
+        speed[1] = -speed[1]
+
     screen.fill(black)
     screen.blit(logo, logorect)
     pygame.display.flip()
