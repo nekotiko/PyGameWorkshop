e90a22bda3a6cf51eab1b04a1ffa2444a863f9c8
Rodolfo Acuna <racuna@growthaccelerationpartners.com>
2015-07-04 15:57:26 -0600

    Solution 01

--- workshop/warmup/warm_up_01.py
+++ workshop/warmup/warm_up_01.py
@@ -17,19 +17,28 @@ pos = [1, 1]
 
 screen = pygame.display.set_mode(size)
 
-logo = pygame.image.load("../assets/mario.png")
-logorect = logo.get_rect()
+mario = pygame.image.load("../assets/mario.png")
+mariorect = mario.get_rect()
 
 while 1:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            pass
 
+    m_pos = pygame.mouse.get_pos()
+
+    mariorect.left, mariorect.top = m_pos
+
+    mario = pygame.transform.scale(mario,
+                                   (int(mariorect.width * SIZE_MULTIPLIER),
+                                    int(mariorect.height * SIZE_MULTIPLIER)))
+
+
+
+    screen.blit(mario, mariorect)
 
 
     screen.fill(black)
-    screen.blit(logo, logorect)
+    screen.blit(mario, mariorect)
     pygame.display.flip()
