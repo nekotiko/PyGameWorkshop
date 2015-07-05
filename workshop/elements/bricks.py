6ba8aad803613edc8ac89b849201f44ae9d99637
Rodolfo Acuna <racuna@growthaccelerationpartners.com>
2015-07-05 09:52:52 -0600

    Solution  03

--- workshop/elements/bricks.py
+++ workshop/elements/bricks.py
@@ -9,7 +9,7 @@ from workshop.utils.sprite_loader import IMAGE_SLIDER
 
 class RedBrick(Sprite):
 
-    def __init__(self, x, y, image='red_floor'):
+    def __init__(self, x, y, image='red_brick'):
         Sprite.__init__(self)
         self.image = IMAGE_SLIDER.get_image(image)
         self.rect = self.image.get_rect()
